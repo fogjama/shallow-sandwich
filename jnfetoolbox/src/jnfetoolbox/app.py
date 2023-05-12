2cad075cdc6d91d9bb38ac5817eab0b82cfe0d2c
James Fogarty <66557609+fogjama@users.noreply.github.com>
2023-05-12 20:56:38 +0100

    Initial setup of beeWare app

---
+++ jnfetoolbox/src/jnfetoolbox/app.py
@@ -0,0 +1,70 @@
+"""
+Toolbox playground application
+"""
+import toga
+import httpx
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+def greeting(name):
+    if name:
+        return f"Hello, {name}"
+    else:
+        return "Hello, stranger"
+
+class JNFEToolbox(toga.App):
+
+    def startup(self):
+        # """
+        # Construct and show the Toga application.
+
+        # Usually, you would add your application to a main content box.
+        # We then create a main window (with a name matching the app), and
+        # show the main window.
+        # """
+        # main_box = toga.Box()
+
+        # self.main_window = toga.MainWindow(title=self.formal_name)
+        # self.main_window.content = main_box
+        # self.main_window.show()
+
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        name_label = toga.Label(
+            "Your name: ",
+            style=Pack(padding=(0, 5))
+        )
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+
+        button = toga.Button(
+            "Say Hello!",
+            on_press=self.say_hello,
+            style=Pack(padding=5)
+        )
+
+        main_box.add(name_box)
+        main_box.add(button)
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+    
+    def say_hello(self, widget):
+        # print(f"Hello, {self.name_input.value}")
+        with httpx.Client() as client:
+            response = client.get("https://jsonplaceholder.typicode.com/posts/42")
+        
+        payload = response.json()
+
+        self.main_window.info_dialog(
+            greeting(self.name_input.value),
+            payload["body"],
+        )
+
+
+def main():
+    return JNFEToolbox()
